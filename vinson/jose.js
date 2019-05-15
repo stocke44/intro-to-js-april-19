ad926789b16506a5e723028c8ef935f28c2a8ca8
Jose Barajas <barajas.jose452yahoo.com>
2019-05-14 19:40:56 -0700

    practice

--- vinson/jose.js
+++ vinson/jose.js
@@ -1,8 +1,51 @@
 (function(){
     const container = document.createElement('div');
     document.body.appendChild(container);
-    container.style['background-color'] = 'aqua';
-    document.body.style.backgroundColor = 'green';
-    container.innerText = 'hello boys and girls';
+    container.style.backgroundColor = '#50A233'
+    container.style.margin = '0';
+    const canvas = document.createElement('canvas');
+    container.appendChild(canvas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const container = document.createElement('div');
+    // document.body.appendChild(container);
+    //  container.style.backgroundImage = 'url(\'./images/space.png\')';
+    //  container.style.backgroundSize = '100% , 500px';
+    //  container.style.backgroundRepeat = 'no-repeat'
+    //  container.style.height = '500px';
+    // // document.body.style.backgroundColor = 'green';
+    // const charSelect = document.createElement('div');
+    // charSelect.style.backgroundColor= 'red';
+    // charSelect.style.height = '20px';
+    // charSelect.style.width ='10%';
+    // container.appendChild(charSelect);
+    
 })();
 
